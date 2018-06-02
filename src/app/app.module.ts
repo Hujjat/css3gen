98d3e2403afd17df61b5a22fa9c8deef9526d861
Hujjat Nazari <Hujjat@users.noreply.github.com>
2018-06-02 11:43:12 +1000

    next up color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +18,8 @@ import { BoxshadowComponent } from './boxshadow/boxshadow.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gradient/gradient.component.ts
+++ src/app/gradient/gradient.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GradientComponent implements OnInit {
 
+  color1 = 'red';
+
   constructor() { }
 
   ngOnInit() {
